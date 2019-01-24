34f5b9f047d61a9b6c0ebbf85dd4de0330d9d81b
Campbell <katiecampbell@DLMDOMXXX012840.local>
2019-01-24 15:45:01 -0800

    reformat tabs add plot

--- server.R
+++ server.R
@@ -5,36 +5,71 @@ library(dplyr)
 library(plotly)
 options(shiny.maxRequestSize=30*1024^2)
 
-myfpkm <- read.table('/Users/katiecampbell/Documents/Projects/M1661/fpkm_matrix.txt', head = T, sep = '\t')
-
 # Define server logic required to plot various variables against mpg
 shinyServer(function(input, output, session) {
-  matrixData <- reactive({
-    # input$fpkm_input will be NULL initially. After the user selects
-    # and uploads a file, it will be a data frame with 'gene_name' 
-    # in the first column, and the remaining columns are samples with FPKM 
-    # expression values
+  fpkmMatrix <- reactive({
     inFile <- input$fpkm_input
     if(is.null(inFile))
       return(NULL)
-    
-    d <- read.table(file = '/Users/katiecampbell/Documents/Projects/M1661/fpkm_matrix.txt', head = TRUE)
+    d <- read.table(file = as.character(inFile$datapath), head = TRUE, sep = input$fpkm_delim)
     return(d)
   })
+  sampleMatrix <- reactive({
+    inFile <- input$sample_input
+    if(is.null(inFile))
+      return(NULL)
+    d <- read.table(file = as.character(inFile$datapath), head = TRUE, sep = input$sample_delim)
+    return(d)
+  })
+  clinMatrix <- reactive({
+    inFile <- input$clin_input
+    if(is.null(inFile))
+      return(NULL)
+    d <- read.table(file = as.character(inFile$datapath), head = TRUE, sep = input$clin_delim)
+    return(d)
+  })
+  # Output data matrices
+  output$fpkm_matrix <- renderDataTable({ fpkmMatrix() }, options = list(pageLength = 10, scrollX = TRUE))
+  output$sample_matrix <- renderDataTable({ sampleMatrix() }, options = list(pageLength = 10, scrollX = TRUE))
+  output$clin_matrix <- renderDataTable({ clinMatrix() }, options = list(pageLength = 10, scrollX = TRUE))
+  # Select genes
   geneOptions <- reactive({
-    if(is.null(matrixData()))
+    if(is.null(fpkmMatrix()))
       selected <- c()
-    
-    selected <- as.character(matrixData()[,1])
+    selected <- as.character(fpkmMatrix()[,1])
     return(selected)
   })
-  
-  output$fpkm_matrix <- renderDataTable({
-    matrixData()
-  }, options = list(pageLength = 10, scrollX = TRUE))
-  
-  output$gene_input <- renderUI({
-    selectizeInput("gene_input", "Select genes", selected = NULL, multiple = TRUE, choices = geneOptions(), options = list(maxItems = 10, placeholder = "Pick your genes"))
+  subsetByGene <- reactive({ 
+    if(is.null(fpkmMatrix()))
+      return(NULL)
+    subsetMatrix <- fpkmMatrix()[which(as.character(fpkmMatrix()[,1]) %in% input$gene_input),] 
+    return(subsetMatrix)
+    })
+  #
+  observe({
+    updateSelectizeInput(session, "gene_input",
+                         choices = geneOptions(), server = TRUE)
   })
-  
+  output$selectedGenes <- renderText({ input$gene_input })
+  output$singleGeneBoxplots <- renderPlotly({
+    if(is.null(subsetByGene()))
+      return(NULL)
+    
+    geneNameColumn <- colnames(subsetByGene())[1]
+    formatData <- subsetByGene() %>% 
+      gather(sample, fpkm, -matches(geneNameColumn))
+    if(input$logFPKM == TRUE){
+      plotY = "log10(fpkm+1)"
+      labelY = "Log FPKM"
+    } else {
+      plotY = "fpkm"
+      labelY = "FPKM"
+    }
+    p <- ggplot(formatData, aes_string(x = geneNameColumn, y = plotY)) +
+      geom_jitter(width = 0.1, alpha = 0.4) + geom_boxplot(outlier.colour = NA, fill = NA) +
+      labs(x = "Gene", y = labelY) +
+      theme_bw() +
+      theme(text = element_text(size = 10), title = element_text(size = 12, face = 'bold'))
+    return(ggplotly(p))
+  })
 })
--- ui.R
+++ ui.R
@@ -1,24 +1,48 @@
 library(shiny)
 
 # Define UI for miles per gallon application
-shinyUI(fluidPage(
-  
-  # Application title
-  headerPanel("Expression Explorer"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      fileInput("fpkm_input", "Choose FPKM matrix input"),
-      uiOutput("gene_input")
-    ),
-    
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Input Matrix",
-                 dataTableOutput("fpkm_matrix"))
-        # tabPanel("Individual Genes",
-        #          plotlyOutput("boxplots"))
-      )
-    )
-  )
-))
+shinyUI(navbarPage("Expression Explorer",
+                   # Displays input data frames
+                   #####
+                   tabPanel("Data",
+                            sidebarLayout(
+                              # Sidebar: Inputs
+                              sidebarPanel(
+                                fileInput("fpkm_input", "Choose FPKM matrix", accept = c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
+                                radioButtons("fpkm_delim", "Select delimiter:", choices = list("Tab" = "\t", "Comma" = ",", "Space" = " "), selected = "\t", inline = TRUE),
+                                fileInput("sample_input", "Choose Sample metadata"),
+                                radioButtons("sample_delim", "Select delimiter:", choices = list("Tab" = "\t", "Comma" = ",", "Space" = " "), selected = "\t", inline = TRUE),
+                                fileInput("clin_input", "Choose Clinial metadata"),
+                                radioButtons("clin_delim", "Select delimiter:", choices = list("Tab" = "\t", "Comma" = ",", "Space" = " "), selected = "\t", inline = TRUE)
+                              ),
+                              # Main panel: Dataframes
+                              mainPanel(
+                                tabsetPanel(
+                                  tabPanel("Gene Expression Matrix",
+                                           dataTableOutput("fpkm_matrix")),
+                                  helpText("Note: Make sure that your expression matrix is formatted appropriately. The first column should contain gene names (each row representing a single gene identifier). Each column is associated with an individual sample (indicated by the column names)."),
+                                  tabPanel("Sample Information",
+                                           dataTableOutput("sample_matrix")),
+                                  tabPanel("Clinical Information",
+                                           dataTableOutput("clin_matrix"))
+                                )
+                              )
+                            )
+                            ),
+                   #####
+                   # Explore without performing statistical analysis
+                   #####
+                   tabPanel("Explore",
+                            sidebarLayout(
+                              # Side bar options: Select genes
+                              # Color by clinical feature
+                              sidebarPanel(
+                                selectizeInput("gene_input", "Select up to 10 gene identifiers:", NULL, multiple = TRUE, options = list(maxOptions = 10)),
+                                checkboxInput("logFPKM", "Log Y axis: ", value = TRUE)
+                              ),
+                              mainPanel(
+                                plotlyOutput("singleGeneBoxplots")
+                              )
+                            )
+                   )
+                   ))
