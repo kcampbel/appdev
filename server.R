14d5aa0d0d37b2f51fb3cd8af812f4dd5643b5ad
Campbell <katiecampbell@dlmdomxxx012840.ad.medctr.ucla.edu>
2019-01-25 10:36:36 -0800

    start adding plots

--- server.R
+++ server.R
@@ -32,6 +32,17 @@ shinyServer(function(input, output, session) {
   output$fpkm_matrix <- renderDataTable({ fpkmMatrix() }, options = list(pageLength = 10, scrollX = TRUE))
   output$sample_matrix <- renderDataTable({ sampleMatrix() }, options = list(pageLength = 10, scrollX = TRUE))
   output$clin_matrix <- renderDataTable({ clinMatrix() }, options = list(pageLength = 10, scrollX = TRUE))
+  # Merge matrices for mapping
+  observe({ updateSelectInput(session, "sampleIdentifier", choices = colnames(sampleMatrix())) })
+  observe({ updateSelectInput(session, "individualIdentifier1", choices = colnames(sampleMatrix())) })
+  observe({ updateSelectInput(session, "individualIdentifier2", choices = colnames(clinMatrix())) })
+  # mapMatrix <- reactive({
+  #   if(is.null(fpkmMatrix())){
+  #     return(NULL)
+  #   } else if(is.null(sampleMatrix())){
+  #     
+  #   }
+  # })
   # Select genes
   geneOptions <- reactive({
     if(is.null(fpkmMatrix()))
@@ -39,25 +50,26 @@ shinyServer(function(input, output, session) {
     selected <- as.character(fpkmMatrix()[,1])
     return(selected)
   })
-  subsetByGene <- reactive({ 
-    if(is.null(fpkmMatrix()))
-      return(NULL)
-    subsetMatrix <- fpkmMatrix()[which(as.character(fpkmMatrix()[,1]) %in% input$gene_input),] 
-    return(subsetMatrix)
-    })
-  #
   observe({
     updateSelectizeInput(session, "gene_input",
                          choices = geneOptions(), server = TRUE)
   })
-  output$selectedGenes <- renderText({ input$gene_input })
-  output$singleGeneBoxplots <- renderPlotly({
-    if(is.null(subsetByGene()))
+  subsetByGene <- reactive({ 
+    if(is.null(fpkmMatrix()))
+      return(NULL)
+    subsetMatrix <- fpkmMatrix()[which(as.character(fpkmMatrix()[,1]) %in% input$gene_input),] 
+    colnames(subsetMatrix)[1] <- "geneName"
+    return(subsetMatrix)
+    })
+  observe({ updateSelectInput(session, "x_corrScatter", choices = input$gene_input) })
+  observe({ updateSelectInput(session, "y_corrScatter", choices = input$gene_input) })
+  # output$selectedGenes <- renderText({ input$gene_input })
+  output$geneBoxplots <- renderPlotly({
+    if(is.null(subsetByGene()) | is.null(input$gene_input))
       return(NULL)
     
-    geneNameColumn <- colnames(subsetByGene())[1]
     formatData <- subsetByGene() %>% 
-      gather(sample, fpkm, -matches(geneNameColumn))
+      gather(colnameFPKM, fpkm, -geneName)
     if(input$logFPKM == TRUE){
       plotY = "log10(fpkm+1)"
       labelY = "Log FPKM"
@@ -65,11 +77,74 @@ shinyServer(function(input, output, session) {
       plotY = "fpkm"
       labelY = "FPKM"
     }
-    p <- ggplot(formatData, aes_string(x = geneNameColumn, y = plotY)) +
-      geom_jitter(width = 0.1, alpha = 0.4) + geom_boxplot(outlier.colour = NA, fill = NA) +
+    q <- ggplot(formatData, aes_string(x = "geneName", y = plotY)) +
+      geom_jitter(width = 0.1, alpha = 0.4, size = 2.5, colour = '#1C4EA1') + geom_boxplot(outlier.shape = 32) +
       labs(x = "Gene", y = labelY) +
       theme_bw() +
       theme(text = element_text(size = 10), title = element_text(size = 12, face = 'bold'))
-    return(ggplotly(p))
+    p <- plotly_build(q)
+    p$x$data <- lapply(p$x$data, FUN = function(s){
+      s$marker$outliercolor = "rgba(0,0,0,0)"
+      s$marker$line$outliercolor = "rgba(0,0,0,0)"
+      return(s)
+    })
+    return(p)
   })
+  output$corrScatter <- renderPlotly({
+    if(is.null(subsetByGene()) | is.null(input$gene_input))
+      return(NULL)
+    
+    formatData <- subsetByGene() %>% filter(geneName %in% c("HLA-A",input$x_corrScatter, input$y_corrScatter)) %>%
+      gather(colnameFPKM, fpkm, -geneName) %>% 
+      mutate(logFPKM = ifelse(is.na(fpkm)==F, log10(fpkm+1), NA))
+    if(input$logFPKM == TRUE){
+      reformatData <- formatData %>% dplyr::select(-fpkm) %>% spread(geneName, logFPKM)
+      labelX = paste0("Log FPKM - ", input$x_corrScatter)
+      labelY = paste0("Log FPKM - ", input$y_corrScatter)
+    } else {
+      reformatData <- formatData %>% dplyr::select(-logFPKM) %>% spread(geneName, fpkm)
+      labelX = paste0("FPKM - ", input$x_corrScatter)
+      labelY = paste0("FPKM - ", input$y_corrScatter)
+    }
+    statistics <- cor.test(reformatData[,as.character(input$x_corrScatter)], reformatData[,as.character(input$y_corrScatter)], method = input$corrMethod)
+    corrLabel <- paste0(input$corrMethod," = ",signif(statistics$estimate,3),"\np = ",signif(statistics$p.value, 3))
+    q <- ggplot(data = reformatData, aes_string(x = paste0("`",input$x_corrScatter,"`"), y = paste0("`",input$y_corrScatter,"`"))) +
+      geom_point(alpha = 0.4, size = 2.5, color = '#1C4EA1') +
+      geom_smooth(size = 0.5, alpha = 0.5, method = 'lm') +
+      geom_text(label = corrLabel, size = 3,
+                x = max(reformatData[,as.character(input$x_corrScatter)], na.rm=T)-(max(reformatData[,as.character(input$x_corrScatter)], na.rm=T)*0.05),
+                y = max(reformatData[,as.character(input$y_corrScatter)], na.rm=T)-(max(reformatData[,as.character(input$y_corrScatter)], na.rm=T)*0.05),
+                hjust = 1, vjust = 1)
+      labs(x = labelX, y = labelY) +
+      theme_bw() +
+      theme(text = element_text(size = 10), title = element_text(size = 12, face = 'bold'))
+    return(plotly_build(q))
+  })
+  output$geneHeatmap <- renderDataTable({
+    if(is.null(subsetByGene()))
+      return(NULL)
+    
+    geneNameColumn <- colnames(subsetByGene())[1]
+    # Get hierarchical clustering of genes and samples
+    m <- subsetByGene() %>% dplyr::select(-geneNameColumn) %>% as.matrix %>% apply(2, as.numeric)
+    row.names(m) <- subsetByGene()[,geneNameColumn]
+    return(m)
+    
+    # formatData <- subsetByGene() %>% 
+    #   gather(sample, fpkm, -matches(geneNameColumn))
+    # if(input$logFPKM == TRUE){
+    #   plotY = "log10(fpkm+1)"
+    #   labelY = "Log FPKM"
+    # } else {
+    #   plotY = "fpkm"
+    #   labelY = "FPKM"
+    # }
+    # p <- ggplot(formatData, aes_string(x = geneNameColumn, y = plotY)) +
+    #   geom_jitter(width = 0.1, alpha = 0.4) + geom_boxplot(outlier.colour = NA, fill = NA) +
+    #   labs(x = "Gene", y = labelY) +
+    #   theme_bw() +
+    #   theme(text = element_text(size = 10), title = element_text(size = 12, face = 'bold'))
+    # return(ggplotly(p))
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -8,22 +8,32 @@ shinyUI(navbarPage("Expression Explorer",
                             sidebarLayout(
                               # Sidebar: Inputs
                               sidebarPanel(
-                                fileInput("fpkm_input", "Choose FPKM matrix", accept = c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
+                                h3("Gene Expression Data"),
+                                fileInput("fpkm_input", "Upload FPKM matrix", accept = c('text/csv', 'text/comma-separated-values,text/plain', '.csv')),
                                 radioButtons("fpkm_delim", "Select delimiter:", choices = list("Tab" = "\t", "Comma" = ",", "Space" = " "), selected = "\t", inline = TRUE),
-                                fileInput("sample_input", "Choose Sample metadata"),
+                                #
+                                h3("Sample Information"),
+                                fileInput("sample_input", "Upload sample metadata"),
                                 radioButtons("sample_delim", "Select delimiter:", choices = list("Tab" = "\t", "Comma" = ",", "Space" = " "), selected = "\t", inline = TRUE),
-                                fileInput("clin_input", "Choose Clinial metadata"),
-                                radioButtons("clin_delim", "Select delimiter:", choices = list("Tab" = "\t", "Comma" = ",", "Space" = " "), selected = "\t", inline = TRUE)
+                                selectInput("sampleIdentifier", "Sample ID: ", NULL, multiple = FALSE),
+                                #
+                                h3("Clinical Information"),
+                                fileInput("clin_input", "Upload clinical metadata"),
+                                radioButtons("clin_delim", "Select delimiter:", choices = list("Tab" = "\t", "Comma" = ",", "Space" = " "), selected = "\t", inline = TRUE),
+                                selectInput("individualIdentifier1", "Map Individual ID (from Sample Information):", NULL, multiple = FALSE),
+                                selectInput("individualIdentifier2", "to Individual ID (from Clinical Information):", NULL, multiple = FALSE)
                               ),
                               # Main panel: Dataframes
                               mainPanel(
                                 tabsetPanel(
                                   tabPanel("Gene Expression Matrix",
+                                           helpText("Note: Make sure that your expression matrix is formatted appropriately. The first column should contain gene names (each row representing a single gene identifier). Each column is associated with an individual sample (indicated by the column names)."),
                                            dataTableOutput("fpkm_matrix")),
-                                  helpText("Note: Make sure that your expression matrix is formatted appropriately. The first column should contain gene names (each row representing a single gene identifier). Each column is associated with an individual sample (indicated by the column names)."),
                                   tabPanel("Sample Information",
+                                           helpText("Note: One column must correspond to the column names (Sample Identifers) indicated in the FPKM matrix."),
                                            dataTableOutput("sample_matrix")),
                                   tabPanel("Clinical Information",
+                                           helpText("Note: There must be at least one column name shared by the Sample and Clinical Metadata."),
                                            dataTableOutput("clin_matrix"))
                                 )
                               )
@@ -37,11 +47,16 @@ shinyUI(navbarPage("Expression Explorer",
                               # Side bar options: Select genes
                               # Color by clinical feature
                               sidebarPanel(
-                                selectizeInput("gene_input", "Select up to 10 gene identifiers:", NULL, multiple = TRUE, options = list(maxOptions = 10)),
-                                checkboxInput("logFPKM", "Log Y axis: ", value = TRUE)
+                                selectizeInput("gene_input", "Select up to 20 gene identifiers:", NULL, multiple = TRUE, options = list(maxOptions = 20)),
+                                checkboxInput("logFPKM", "Log FPKM", value = TRUE),
+                                selectInput("x_corrScatter", "X axis", NULL, multiple = FALSE),
+                                selectInput("y_corrScatter", "Y axis", NULL, multiple = FALSE),
+                                radioButtons("corrMethod", "Correlation method: ", choices = list("Pearson" = "pearson", "Kendall" = "kendall", "Spearman" = "spearman"), selected = "pearson", inline = TRUE)
                               ),
                               mainPanel(
-                                plotlyOutput("singleGeneBoxplots")
+                                plotlyOutput("geneBoxplots"),
+                                plotlyOutput("corrScatter")
+                                # dataTableOutput("geneHeatmap")
                               )
                             )
                    )
